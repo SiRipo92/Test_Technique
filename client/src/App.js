f57eb160e33933bb9a1f2c6b180150b5aa54baa6
SiRipo92 <ripochesierra@gmail.com>
2025-02-05 14:58:29 +0100

    Modified App entry point code to import To-Do list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Todo from "./components/Todo"; // Import To-Do component
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> {/* Main container for the application */}
+      <h1>To-Do List</h1>
+      <Todo /> {/* Render the To-Do component inside App */}
     </div>
   );
 }
 
+// Export the App component so it can used in index.js
 export default App;
