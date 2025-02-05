99dbda08a02eeb4462a2eae2a0a911fefc77ad52
SiRipo92 <ripochesierra@gmail.com>
2025-02-05 15:22:00 +0100

    Added input state for new task in ToDo list

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,9 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react"; // Import useState
 
 const Todo = () => {
+
+    const [newTodo, setNewTodo] = useState(""); // State for input field
+
     return (
         <div>
             <h2>What do you have to do?</h2>
+            {/* Input field for new task */}
+            <input
+                type="text"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)} // Update state when user types
+                placeholder="Add a new task..."
+            />
+            <button>
+                Add task
+            </button>
         </div>
     );
 };
