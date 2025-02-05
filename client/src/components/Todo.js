40dfe8286b13424bea8a626833ad8f5046c8e8fe
SiRipo92 <ripochesierra@gmail.com>
2025-02-05 15:15:34 +0100

    Built basic component structure for To-Do List

---
+++ client/src/components/Todo.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const Todo = () => {
+    return (
+        <div>
+            <h2>What do you have to do?</h2>
+        </div>
+    );
+};
+
+export default Todo; //Export the component
